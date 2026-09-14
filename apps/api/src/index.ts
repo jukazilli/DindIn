4724b9f9875bff397105c2ae833aa04334b23474
Juliano Zilli <107819877+jukazilli@users.noreply.github.com>
2026-09-14 11:14:05 -0300

    feat(api): export database composition root

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./adapters/drizzle-store";
 export * from "./application/dindin-service";
 export * from "./application/errors";
+export * from "./composition";
 export * from "./http/app";
 export * from "./ports/store";
